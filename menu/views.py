888e8ba16ac60be4299ccd56d59d180f6167b27f
vidyabombothula <vidyasribombothula@gmail.com>
2025-08-29 16:01:43 +0530

    36361

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import DishSerializer
+from django.shortcuts import render
+import requests
+
+def home(request):
+    try:
+
+        response=requests.get()
+        menu_items=response.json()
+    except Execption as e:
+        menu_items=[]
+        print("Error fetching menu: {e}")
+
+    return render(request, 'menu/home.html', {'menu_items':menu_items})
 
 class MenuAPIView(APIView):
     def get(self,request):
