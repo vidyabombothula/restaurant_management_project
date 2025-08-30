a62a755675fb568fed011fd35349ff1044ab1c84
vidyabombothula <vidyasribombothula@gmail.com>
2025-08-30 13:07:37 +0530

    40686

---
+++ restaurant/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class MenuItems(models.Model):
+    name=models.CharField(max_length=100)
+    price=models.DecimalField(max_digits=6,decimal_places=2)
+
+    def __str__(self):
+        return self.name
